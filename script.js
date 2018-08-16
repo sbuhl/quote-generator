c1d8fc2785ea08e4bf6108ffe7c7cf7b081b30c2
Cerrer <s.buhl@hotmail.com>
2018-08-16 16:46:48 +0200

    test

--- script.js
+++ script.js
@@ -1,15 +1,35 @@
-console.log("Welcome to your quotation manager !")
+'use strict';
 
- function presentChoices(){
-     console.log("1: List of quotes")
-     console.log("2: Random quote")
-     console.log("3: Add quote")
-     console.log("0: Quit")
- }
-document.getElementById("displayQuotes").addEventListener("click", displayQuotes);
+document.getElementById("refresh").addEventListener("click", refresh);
+
+function refresh() {
+    location.reload();
+}
+
+document.getElementById("displayAllQuotes").addEventListener("click", displayAllQuotes);
+
+function displayAllQuotes(){
+    var allQuotes = document.getElementById("toHide displayQuotes");
+    allQuotes.id = "toHide displayQuotes";
+    allQuotes.classList.remove("hide");
+    
+    quotationsList.forEach(function(quotation){
+        document.getElementById("displayQuotes-inside").innerHTML += quotation.QuotationsStore.describe();
+    });
+
+    console.log("Here's the list of all your quotes :");
+    quotationsList.forEach(function(quotation){
+        var i=0;
+        i++;
+        console.log(quotation.describe());
+    });
+};
+
+
+
+
 document.getElementById("randomQuotes").addEventListener("click", randomQuotes);
 document.getElementById("displayForm").addEventListener("click", displayForm);
-document.getElementById("refresh").addEventListener("click", refresh);
 document.getElementById("quotesProgram").addEventListener("click", quotesProgram);
 document.getElementById("removeForm").addEventListener("click", removeForm);
 document.getElementById("createQuote").addEventListener("click", createQuote);
@@ -19,25 +39,171 @@ document.getElementById("removeForm").addEventListener("click", removeForm);
 document.getElementById("removeRandomQuotes").addEventListener("click", removeRandomQuotes);
 
 var quotations = new Array();
-function Quotation(quote, category, character, title, year){
-    this.quote      = quote;
-    this.category   = category;
-    this.character  = character;
-    this.title      = title;
-    this.year       = year;
-}
-Quotation.prototype.describe = function(){
-    var description = 
-        `   <tr class="parent">
-                <td class="child">${this.quote}</td>
-                <td class="child">${this.category}</td>
-                <td class="child">${this.character}</td>
-                <td class="child">${this.title}</td>
-                <td class="child">${this.year}</td>
-            </tr>
-        `
-    return description;
-}
+
+function Quotation(type, quotes, characters, titles, years){
+    this.type        = type;
+    this.characters  = characters;
+    this.quotes      = quotes;
+    this.titles      = titles;
+    this.years       = years;
+};
+
+class Quote{
+    constructor(character, quote, title, year){
+        this.character = character;
+        this.quote = quote;
+        this.title = title;
+        this.year = year;
+    }
+};
+
+class QuotationsStore {
+    constructor(){
+        this.quotationsList = [];
+    }
+
+    add(quotations){
+        this.quotationsList.push(quotations);
+    }
+
+    initialStorage(){
+        const moviesQuotes = new Quotation('Movie',
+        [
+            'On devient pas chef',
+	        'Nous on est Celtes',
+	        'Une fois j\'ai dormi avec un porc',
+	        'Tu peux préparer les outils',
+	        'Tu devais aller en Germanie',
+	        'Vous connaissez pas le jeu du pélican',
+	        'Croyez moi, c\'est pour votre bien',
+	        'C\'est vous qui faites du bruit',
+	        'Donc si je comprend bien',
+	        'Il faut quand même qu\'on sache sur quel pied danser'
+        ],
+        [
+            ' parcequ\'on le mérite',
+	        ' une seule année dans la légion',
+	        ' pendant une semaine',
+	        ', allez trouve un Breton',
+	        ', je connais à mort',
+	        ' si vous préférez pas jouer à autre chose ',
+	        ', ça va être la grande rigolade pendant quatre jours',
+	        ', je savais pas que c\'était aussi clair',
+	        ', vous voyez bien que c\'est un piège',
+	        ' faites pas celui qu\'est pas au courant'
+        ],
+        [
+            ' andouille !',
+	        ', ça peut encore s\'arranger...',
+	        ', je serais vous, je lancerais des recherches.',
+	        ', c’est moi qui paye l’orchestre.',
+	        ', c\'est vrai ou c\'est pas vrai ?',
+	        ', une petite partie de dés ça peut pas nous faire de mal.',
+	        ', ça doit venir du fait qu\'on marche...',
+	        ', pas de vannes, pas de réflexions...',
+	        ', hé ben, c\'est pas gentil, voila.',
+	        ' bah je sais pas comment vous faites, moi je pourrais pas.'
+        ],
+        [
+            ' andouille !',
+	        ', ça peut encore s\'arranger...',
+	        ', je serais vous, je lancerais des recherches.',
+	        ', c’est moi qui paye l’orchestre.',
+	        ', c\'est vrai ou c\'est pas vrai ?',
+	        ', une petite partie de dés ça peut pas nous faire de mal.',
+	        ', ça doit venir du fait qu\'on marche...',
+	        ', pas de vannes, pas de réflexions...',
+	        ', hé ben, c\'est pas gentil, voila.',
+	        ' bah je sais pas comment vous faites, moi je pourrais pas.'
+        ]);
+        const booksQuotes = new Quotation('Books',
+        [
+            'Dans la vie on ne fait pas ce que l\'on veut', 
+			'La vie est un mystère qu\'il faut vivre', 
+			'On passe une moitié de sa vie à attendre', 
+			'La vie, c\'est comme une bicyclette', 
+			'Dans la nature, tout a toujours une raison',
+			'Notre plus grande gloire n\'est point de tomber', 
+			'Certains ont l\'air honnête', 
+			'Etre libre, ce n\'est pas seulement se débarrasser de ses chaînes', 
+			'Le courage, c\'est de comprendre sa propre vie', 
+			'L\'éducation ne se borne pas à l\'enfance'
+        ],
+        [
+            ' mais on est responsable', 
+			'. Non pas tant à cause de ceux qui font le mal', 
+			' mais donne autant de lumière qu\'un sourire', 
+			' ils ne se rendent jamais compte de leurs souffrances', 
+			', tout obstacle renforce la détermination',
+			', je cherche à comprendre les questions', 
+			' car la promesse est une dette', 
+			', c\'est quand tout fonctionne et que personne ne sait pourquoi', 
+			' mais j\'en suis fier', 
+			' et s\'attendre à un résultat différent'
+        ],
+        [
+            ' à cause de ceux qui regardent et laissent faire.', 
+			' pour ne pas perdre l\'équilibre.', 
+			' un seul jour de votre vie.', 
+			', ça n\'est pas forcément le pot qui est vide.', 
+			', les enfants sont des tyrans.',
+			' chaque fois que nous tombons.', 
+			' mais la capacité de la vaincre.', 
+			', tout le reste c\'est de travers.', 
+			'. Il n\'y a que de mauvais cultivateurs.', 
+			'. J\'avais déjà du mal avec la main !'
+        ],
+        [
+            ' à cause de ceux qui regardent et laissent faire.', 
+			' pour ne pas perdre l\'équilibre.', 
+			' un seul jour de votre vie.', 
+			', ça n\'est pas forcément le pot qui est vide.', 
+			', les enfants sont des tyrans.',
+			' chaque fois que nous tombons.', 
+			' mais la capacité de la vaincre.', 
+			', tout le reste c\'est de travers.', 
+			'. Il n\'y a que de mauvais cultivateurs.', 
+			'. J\'avais déjà du mal avec la main !'
+        ]);
+        this.add(moviesQuotes);
+        this.add(booksQuotes);
+        console.log(booksQuotes);
+    }
+
+    describe(){
+        var description = 
+            `   <tr class="parent">
+                    <td class="child">${this.quote}</td>
+                    <td class="child">${this.category}</td>
+                    <td class="child">${this.character}</td>
+                    <td class="child">${this.title}</td>
+                    <td class="child">${this.year}</td>
+                </tr>
+            `
+        return description;
+    }
+};
+
+class QuoteGenerator {
+    
+
+};
+
+
+//Quotation.prototype.describe = function(){
+//    var description = 
+//        `   <tr class="parent">
+//                <td class="child">${this.quote}</td>
+//                <td class="child">${this.category}</td>
+//                <td class="child">${this.character}</td>
+//                <td class="child">${this.title}</td>
+//                <td class="child">${this.year}</td>
+//            </tr>
+//        `
+//    return description;
+//}
+
+
 // Creation of the five first quotations
 var jurrasicPark = new Quotation(
     "Your scientists were so preoccupied with whether they could, they didn't stop to think if they should.",
