6f426cbb907e74dc84759a5ad9c9ab6c9d81392c
Cerrer <s.buhl@hotmail.com>
2018-08-17 10:32:27 +0200

    More object oriented

--- script.js
+++ script.js
@@ -1,117 +1,161 @@
-document.getElementById("displayAllQuotes").addEventListener("click", displayQuotes);
-document.getElementById("randomMovie").addEventListener("click", randomMovie);
-document.getElementById("randomBook").addEventListener("click", randomBook);
+// Creation of the quotations
+class QuotationRepository {
+    constructor(){
+        this.quotationTable = [];
+    }
+    add(quotationSample){
+        this.quotationTable.push(quotationSample);
+    }
+};
 
-document.getElementById("displayForm").addEventListener("click", displayForm);
-document.getElementById("quotesProgram").addEventListener("click", quotesProgram);
-document.getElementById("removeForm").addEventListener("click", removeForm);
-document.getElementById("createQuote").addEventListener("click", createQuote);
-document.getElementById("removeAllQuotes").addEventListener("click", removeAllQuotes);
-document.getElementById("removeForm").addEventListener("click", removeForm);
-document.getElementById("removeRandomQuotes").addEventListener("click", removeRandomQuotes);
+class QuotationSample {
+    constructor(category, quote, character, title, year){
+        this.category   = category;
+        this.quote      = quote;
+        this.character  = character;
+        this.title      = title;
+        this.year       = year;    
+    };
 
-var quotations = new Array();
-function Quotation(quote, category, character, title, year){
-    this.quote      = quote;
-    this.category   = category;
-    this.character  = character;
-    this.title      = title;
-    this.year       = year;
+    fillQuoteStorage(){
+        const movies = new QuotationSample('Movie',     
+        [
+            'Your scientists were so preoccupied with whether they could, they didn\'t stop to think if they should.',
+            'Oh shit this is cold! Shit, shit, shit.',
+            'Shut up ! Just shut up, you idiot!',
+            'Frankly, my dear, I don\'t give a damn.',
+            'I\'m going to make him an offer he can\'t refuse.',
+        ],
+        [
+            'Ian Malcom',
+            'Jack Dawson',
+            'Woody',
+            'Rhett Butler',
+            'Don Corleone',
+        ],
+        [
+            'Jurassic Park',
+            'Titanic',
+            'Toy Story',
+            'Gone With the Wind',
+            'The Godfather'
+        ],
+        [
+            1993,
+            1997,
+            1995,
+            1939,
+            1972
+        ]
+        );
+    
+        const books = new QuotationSample('Book',
+        [
+            'Human speech is like a cracked kettle on which we tap crude rhythms for bears to dance to, while we long to make music that will melt the stars.',
+            'Oh shit this is cold! Shit, shit, shit.',
+            'Je n\'utilise pas seulement mes neurones mais tous ceux que je peux emprunter',
+            'With all the wonders Moses was performing, there was one I did not think would ever come to pass: that we would finally be reunited. That I would once again kiss his neck as I had loved to do. That I would see him clasp his sons to his breast.',
+            'The more extreme the opening positions and the smaller the concessions, the more time and effort it will take to discover whether or not agreement is possible.',
+        ],
+        [
+            'Gustave Flaubert',
+            'Jack Dawson',
+            'Tim Ferris',
+            'Marek Halter',
+            'Roger Fisher',
+        ],
+        [
+            'Madame Bovary',
+            'Le Horla',
+            'La semaine de 4 heures',
+            'Khadija',
+            'Getting to yes',
+        ],
+        [
+            1993,
+            1997,
+            1995,
+            1939,
+            1972
+        ]
+        );
+
+        this.XXXXXX.add(movies);
+        this.XXXXXX.add(books);
+    };
+    
+};
+
+
+
+
+// OPTION 1 :  Refresh the page
+document.getElementById("refresh").addEventListener("click", refresh);
+function refresh() {
+    location.reload();
 }
 
-// Creation of the five first Movies
-var jurrasicPark = new Quotation(
-    "Your scientists were so preoccupied with whether they could, they didn't stop to think if they should.",
-    "Movie",
-    "Ian Malcom",
-    "Jurassic Park",
-    1993); 
-var titanic = new Quotation(
-    "Oh shit this is cold! Shit, shit, shit.",
-    "Movie",
-    "Jack Dawson",
-    "Titanic",
-    1997); 
-var toyStory = new Quotation(
-    "Shut up ! Just shut up, you idiot!",
-    "Movie",
-    "Woody",
-    "Toy Story",
-    1995); 
-var goneWith = new Quotation(
-    "Frankly, my dear, I don't give a damn.",
-    "Movie",
-    "Rhett Butler",
-    "Gone With the Wind",
-    1939); 
-var godFather = new Quotation(
-    "I'm going to make him an offer he can't refuse.",
-    "Movie",
-    "Don Corleone",
-    "The Godfather",
-    1972); 
-quotations.push(jurrasicPark);
-quotations.push(titanic);
-quotations.push(toyStory);
-quotations.push(goneWith);
-quotations.push(godFather);
+// OPTION 2 : Display all the quotes in a table - NOT RANDOMLY
+document.getElementById("displayAllQuotes").addEventListener("click", displayQuotes);
+function displayQuotes(){
+    var allQuotes = document.getElementById("toHide displayQuotes");
+    allQuotes.id = "toHide displayQuotes";
+    allQuotes.classList.remove("hide");
+    
+    quotations.forEach(function(quotation){
+        document.getElementById("displayQuotes-inside").innerHTML += quotation.describe();
+    });
 
-// Creation of the five first Book
-var madameBovary = new Quotation(
-    "Human speech is like a cracked kettle on which we tap crude rhythms for bears to dance to, while we long to make music that will melt the stars.",
-    "Book",
-    "Gustave Flaubert",
-    "Madame Bovary",
-    1993); 
-var leHorla = new Quotation(
-    "Oh shit this is cold! Shit, shit, shit.",
-    "Book",
-    "Jack Dawson",
-    "Le Horla",
-    1997); 
-var semaineQuatre = new Quotation(
-    "Je n’utilise pas seulement mes neurones mais tous ceux que je peux emprunter",
-    "Book",
-    "Tim Ferris",
-    "La semaine de 4 heures",
-    1995); 
-var khadija = new Quotation(
-    "With all the wonders Moses was performing, there was one I did not think would ever come to pass: that we would finally be reunited. That I would once again kiss his neck as I had loved to do. That I would see him clasp his sons to his breast.",
-    "Book",
-    "Marek Halter",
-    "Khadija",
-    1939); 
-var fatima = new Quotation(
-    "The more extreme the opening positions and the smaller the concessions, the more time and effort it will take to discover whether or not agreement is possible.",
-    "Book",
-    "Roger Fisher",
-    "Getting to yes",
-    1972); 
-quotations.push(madameBovary);
-quotations.push(leHorla);
-quotations.push(semaineQuatre);
-quotations.push(khadija);
-quotations.push(fatima);
+    quotations.forEach(function(quotation){
+        var i=0;
+        i++;
+    });
+}
 
+// OPTION 3&4 : Display the Movies OR the Books Randomly
+document.getElementById("randomMovie").addEventListener("click", randomQuote);
+document.getElementById("randomBook").addEventListener("click", randomQuote);
+
+function randomQuote(){
+    var randomType = (this.name);
+    var numberOfQuotes = "";
+    numberOfQuotes = prompt('How many quotes do you want ? (between 1 and 5) :');
+    if(numberOfQuotes > 5 || numberOfQuotes <= 0){
+        console.log("You can't write this number. Please, try again.")
+    }else { 
+        var randomQuotes = document.getElementById("toHide randomQuote");
+        randomQuotes.id = "toHide randomQuote";
+        randomQuotes.classList.remove("hide");
+        
+        const randomTypeClicked = quotations.filter(function(quotation){
+            if(quotation.category === randomType){
+                console.log(randomTypeClicked);
+                return true;
+            }
+        });
+
+        for (var counter = 0; counter < numberOfQuotes; counter++) {
+            var quoteR      = randomTypeClicked[Math.floor(Math.random()*randomTypeClicked.length)].quote;
+            var characterR  = randomTypeClicked[Math.floor(Math.random()*randomTypeClicked.length)].character;
+            var titleR      = randomTypeClicked[Math.floor(Math.random()*randomTypeClicked.length)].title;
+            var yearR       = randomTypeClicked[Math.floor(Math.random()*randomTypeClicked.length)].year;
+            document.getElementById("display-random-quotes").innerHTML += 
+            `   <p class="mb-0">${quoteR}</p>
+                <footer class="blockquote-footer">${characterR} <cite title="Source Title">${titleR} - ${yearR}</cite></footer>
+            `;
+        }
+    }
+}
+
+// OPTION 5 : Add a quote
+document.getElementById("displayForm").addEventListener("click", displayForm);
 function displayForm(){
     var createQuotes = document.getElementById("toHide createQuote");
     createQuotes.id = "toHide createQuote";
     createQuotes.classList.remove("hide");    
 }
-function removeForm(){
-    var toHide = document.getElementById("toHide createQuote");
-    toHide.classList.add("hide");
-    document.getElementById("quote").value = "";    
-    document.getElementById("category").value = "";    
-    document.getElementById("title").value = "";    
-    document.getElementById("character").value = "";   
-    document.getElementById("year").value = "";  
-}
 
-function capitalizeFirstLetter(ifno) {
-    return ifno.charAt(0).toUpperCase() + ifno.slice(1);
-}
-
+document.getElementById("createQuote").addEventListener("click", createQuote);
 function createQuote(){
     var newQuote = document.getElementById("quote").value.toLowerCase()
     newQuote = capitalizeFirstLetter(newQuote);    
@@ -129,29 +173,43 @@ function createQuote(){
     return quotations.push(newQuotation);
 }
 
+
+document.getElementById("quotesProgram").addEventListener("click", quotesProgram);
+document.getElementById("removeForm").addEventListener("click", removeForm);
+document.getElementById("removeAllQuotes").addEventListener("click", removeAllQuotes);
+document.getElementById("removeForm").addEventListener("click", removeForm);
+document.getElementById("removeRandomQuotes").addEventListener("click", removeRandomQuotes);
+
+
+
+
+
+
+
+
+function removeForm(){
+    var toHide = document.getElementById("toHide createQuote");
+    toHide.classList.add("hide");
+    document.getElementById("quote").value = "";    
+    document.getElementById("category").value = "";    
+    document.getElementById("title").value = "";    
+    document.getElementById("character").value = "";   
+    document.getElementById("year").value = "";  
+}
+
+function capitalizeFirstLetter(ifno) {
+    return ifno.charAt(0).toUpperCase() + ifno.slice(1);
+}
+
+
+
 function removeAllQuotes(){
     var toHide = document.getElementById("toHide displayQuotes");
     toHide.classList.add("hide");
     document.getElementById("displayQuotes-inside").innerHTML = "";
 }
 
-function displayQuotes(){
-    var allQuotes = document.getElementById("toHide displayQuotes");
-    allQuotes.id = "toHide displayQuotes";
-    allQuotes.classList.remove("hide");
-    
-    quotations.forEach(function(quotation){
-        document.getElementById("displayQuotes-inside").innerHTML += quotation.describe();
-    });
 
-    console.log("Here's the list of all your quotes :");
-    quotations.forEach(function(quotation){
-        var i=0;
-        i++;
-    });
-    console.table(quotations);
-
-}
 
 function removeRandomQuotes(){
     var toHide = document.getElementById("toHide randomQuote");
@@ -159,102 +217,26 @@ function removeRandomQuotes(){
     document.getElementById("display-random-quotes").innerHTML = "";
 }
 
-Quotation.prototype.describe = function(){
-    var description = 
-        `   <tr class="parent">
-                <td class="child">${this.quote}</td>
-                <td class="child">${this.category}</td>
-                <td class="child">${this.character}</td>
-                <td class="child">${this.title}</td>
-                <td class="child">${this.year}</td>
-            </tr>
-        `
-    return description;
-}
+//Quotation.prototype.describe = function(){
+//    var description = 
+//        `   <tr class="parent">
+//                <td class="child">${this.quote}</td>
+//                <td class="child">${this.category}</td>
+//                <td class="child">${this.character}</td>
+//                <td class="child">${this.title}</td>
+//                <td class="child">${this.year}</td>
+//            </tr>
+//       `
+//    return description;
+//}
 
-function randomMovie(){
-    var numberOfQuotes = "";
-    numberOfQuotes = prompt('How many quotes do you want ? (between 1 and 5) :');
-    if(numberOfQuotes > 5 || numberOfQuotes <= 0){
-        console.log("You can't write this number. Please, try again.")
-        randomQuotes();
-    }else { 
-        var randomQuotes = document.getElementById("toHide randomQuote");
-        randomQuotes.id = "toHide randomQuote";
-        randomQuotes.classList.remove("hide");
-        
-        const films = quotations.filter(function(quotation){
-            if(quotation.category === "Movie"){
-                return true;
-            }
-        });
-        
-        console.table(films);
 
-        for (var counter = 0; counter < numberOfQuotes; counter++) {
-            var quoteR = films[Math.floor(Math.random()*films.length)].quote;
-            var characterR = films[Math.floor(Math.random()*films.length)].character;
-            var categoryR = films[Math.floor(Math.random()*films.length)].category;
-            var titleR = films[Math.floor(Math.random()*films.length)].title;
-            var yearR = films[Math.floor(Math.random()*films.length)].year;
-            console.log(quoteR, characterR, titleR, yearR);
-            document.getElementById("display-random-quotes").innerHTML += 
-            `   <tr class="parent">
-                    <td class="child">${quoteR}</td>
-                    <td class="child">${categoryR}</td>
-                    <td class="child">${characterR}</td>
-                    <td class="child">${titleR}</td>
-                    <td class="child">${yearR}</td>
-                </tr>
-            `;
-        }
-    }
-}
 
-function randomBook(){
-    var numberOfQuotes = "";
-    numberOfQuotes = prompt('How many quotes do you want ? (between 1 and 5) :');
-    if(numberOfQuotes > 5 || numberOfQuotes <= 0){
-        console.log("You can't write this number. Please, try again.")
-        randomQuotes();
-    }else { 
-    
-        var randomQuotes = document.getElementById("toHide randomQuote");
-        randomQuotes.id = "toHide randomQuote";
-        randomQuotes.classList.remove("hide");
-        
-        const livres = quotations.filter(function(quotation){
-            if(quotation.category === "Book"){
-                return true;
-            }
-        });
-        
-        console.table(livres);
 
-        for (var counter = 0; counter < numberOfQuotes; counter++) {
-            var quoteR = livres[Math.floor(Math.random()*livres.length)].quote;
-            var characterR = livres[Math.floor(Math.random()*livres.length)].character;
-            var categoryR = livres[Math.floor(Math.random()*livres.length)].category;
-            var titleR = livres[Math.floor(Math.random()*livres.length)].title;
-            var yearR = livres[Math.floor(Math.random()*livres.length)].year;
-            console.log(quoteR, characterR, titleR, yearR);
-            document.getElementById("display-random-quotes").innerHTML += 
-            `   <tr class="parent">
-                    <td class="child">${quoteR}</td>
-                    <td class="child">${categoryR}</td>
-                    <td class="child">${characterR}</td>
-                    <td class="child">${titleR}</td>
-                    <td class="child">${yearR}</td>
-                </tr>
-            `;
-        }
-    }
 
-}
 
-function refresh() {
-    location.reload();
-}
+
+
 
 function quotesProgram(){
     var valeur = document.getElementById("valeur").value
